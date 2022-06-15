98d6950033230e2a30145c2308d9ff17b2d74081
samah anwer alsatary <alsatary.samah@yahoo.com>
2022-06-15 12:16:41 +0300

    dev

---
+++ src/auth/models/users.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const bcrypt = require('bcrypt');
+const jwt=require('jsonwebtoken')
+require("dotenv").config();
+
+const userSchema = (sequelize, DataTypes) => {
+  const model = sequelize.define('samah', {
+    username: { type: DataTypes.STRING, allowNull: false, unique: true },
+    password: { type: DataTypes.STRING, allowNull: false},
+
+    
+    token: {
+      type: DataTypes.VIRTUAL,
+      get() {
+        return jwt.sign({ username: this.username },process.env.SECRET);
+      }
+    }
+  });
+
+  model.beforeCreate(async (user) => {
+    let hashedPass = bcrypt.hash(user.password, 10);
+    user.password = hashedPass;
+  });
+
+  // Basic AUTH: Validating strings (username, password) 
+  model.authenticateBasic = async function (username, password) {
+  
+    const user = await this.findOne({ where: { username: username } })
+    
+    // console.log("pass "+password);
+    // console.log("user  "+user.password)
+    const valid = await bcrypt.compare(password, user.password)
+    console.log("valid",valid);
+    
+    if (valid) {
+
+      let newToken = jwt.sign({ username: user.username },process.env.SECRET);
+        // console.log('************************', newToken);
+        user.token = newToken;
+       return user; 
+      }
+   
+    throw new Error('Invalid User');
+  }
+ 
+  // Bearer AUTH: Validating a token
+  model.authenticateToken = async function (token) {
+    try {
+      const parsedToken = jwt.verify(token, process.env.SECRET);
+      const user =await this.findOne({ username: parsedToken.username })
+      if (user.username) { return user; }
+      throw new Error("User Not Found");
+    } catch (e) {
+      console.log(e)
+      throw new Error(e.message)
+    }
+  }
+
+  return model;
+}
+
+module.exports = userSchema;
